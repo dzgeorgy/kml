b3b1a831ceaf97f8bc8fbc42ce5305bae39c2b7a
Georgy Dzutsev <dzgeorgy@icloud.com>
2024-10-11 14:45:19 +0300

    test: write samples

--- sample/src/main/kotlin/dev/dzgeorgy/kml/Examples.kt
+++ sample/src/main/kotlin/dev/dzgeorgy/kml/Examples.kt
@@ -1,5 +1,32 @@
 package dev.dzgeorgy.kml
 
+import dev.dzgeorgy.kml.ext.TrackEntity
+
 fun main() {
     println("Test")
+
+    val track = Track("Blank Space", 3.51f, "Taylor Swift")
+    val trackEntity = TrackEntity("Blank Space", 3.51f, "Taylor Swift")
+    val trackModel = TrackModel("Blank Space", 3.51f, "Taylor Swift")
+    println(track.toTrackEntity())
+    println(track.toTrackModel())
+//    println(trackEntity.toTrackModel())
+}
+
+@Map(TrackEntity::class)
+@Map(TrackModel::class)
+data class Track(
+    val name: String,
+    var length: Float,
+    val author: String
+)
+
+class TrackModel(
+    val name: CharSequence,
+    val length: Float,
+    val author: CharSequence
+) {
+    override fun toString(): String {
+        return "TrackModel(name=$name, length=$length, author=$author)"
+    }
 }
---
+++ sample_external/src/main/kotlin/dev/dzgeorgy/kml/ext/TrackEntity.kt
@@ -0,0 +1,7 @@
+package dev.dzgeorgy.kml.ext
+
+data class TrackEntity(
+    val name: CharSequence,
+    val length: Float,
+    var author: CharSequence
+)
